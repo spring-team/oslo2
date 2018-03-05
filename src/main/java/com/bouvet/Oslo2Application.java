2cd8d22fa0b2a7db82add2f2f516be90dbb6433b
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 17:10:05 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/bouvet/Oslo2Application.java
@@ -0,0 +1,12 @@
+package com.bouvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oslo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oslo2Application.class, args);
+	}
+}
